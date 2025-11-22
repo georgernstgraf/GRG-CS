7f6de14199784f907c57b19d9b9399a34fa6a08e
Georg Graf (SPG) (acer) <grafg@spengergasse.at>
2025-11-22 19:35:33 +0100

    reformat

--- 3ahwii/2025-11_brueche/Bruch.cs
+++ 3ahwii/2025-11_brueche/Bruch.cs
@@ -1,59 +1,59 @@
 namespace _2025_11_brueche;
 
 internal class Bruch
+{
+    // jetzt kommen die sog. "Attribute" der Klasse oder "Felder"
+    private int _ganz;
+    private int _nenner;
+    private int _zaehler;
+
+    public Bruch(string bruchtext)
     {
-        // jetzt kommen die sog. "Attribute" der Klasse oder "Felder"
-        private int _ganz;
-        private int _zaehler;
-        private int _nenner;
-
-        public Bruch(string bruchtext)
-        {
-            var teile1 = bruchtext.Split(' ');
-            this._ganz = int.Parse(teile1[0]);
-            var teile = teile1[1].Split('/');
-            this._zaehler = int.Parse(teile[0]);
-            this._nenner = int.Parse(teile[1]);
-        }
-
-        private Bruch(int ganz, int zaehler, int nenner)
-        {
-            this._ganz = ganz;
-            this._zaehler = zaehler;
-            this._nenner = nenner;
-            this.Kuerze();
-        }
-
-        private void Kuerze()
-        {
-            var g = Gcd(this._zaehler, this._nenner);
-            this._zaehler /= g;
-            this._nenner /= g;
-            this._ganz += this._zaehler / this._nenner;
-            this._zaehler %= this._nenner;
-        }
-
-        private static int Gcd(int a, int b)
-        {
-            while (b != 0)
-            {
-                int temp = b;
-                b = a % b;
-                a = temp;
-            }
-
-            return a;
-        }
-
-        public Bruch Addiere(Bruch b)
-        {
-            var a = this;
-            return new Bruch(a._ganz + b._ganz, a._zaehler * b._nenner + b._zaehler * a._nenner, a._nenner * b._nenner);
-        }
-
-        public override string ToString()
-        {
-            return $"{this._ganz} {this._zaehler}/{this._nenner}";
-            // JS: return `ich bin ein bruch: ${this.zaehler}/${this.nenner}`;
-        }
+        var teile1 = bruchtext.Split(' ');
+        _ganz = int.Parse(teile1[0]);
+        var teile = teile1[1].Split('/');
+        _zaehler = int.Parse(teile[0]);
+        _nenner = int.Parse(teile[1]);
     }
+
+    private Bruch(int ganz, int zaehler, int nenner)
+    {
+        _ganz = ganz;
+        _zaehler = zaehler;
+        _nenner = nenner;
+        Kuerze();
+    }
+
+    private void Kuerze()
+    {
+        var g = Gcd(_zaehler, _nenner);
+        _zaehler /= g;
+        _nenner /= g;
+        _ganz += _zaehler / _nenner;
+        _zaehler %= _nenner;
+    }
+
+    private static int Gcd(int a, int b)
+    {
+        while (b != 0)
+        {
+            var temp = b;
+            b = a % b;
+            a = temp;
+        }
+
+        return a;
+    }
+
+    public Bruch Addiere(Bruch b)
+    {
+        var a = this;
+        return new Bruch(a._ganz + b._ganz, a._zaehler * b._nenner + b._zaehler * a._nenner, a._nenner * b._nenner);
+    }
+
+    public override string ToString()
+    {
+        return $"{_ganz} {_zaehler}/{_nenner}";
+        // JS: return `ich bin ein bruch: ${this.zaehler}/${this.nenner}`;
+    }
+}
--- 3ahwii/2025-11_brueche/Program.cs
+++ 3ahwii/2025-11_brueche/Program.cs
@@ -1,17 +1,15 @@
 ﻿namespace _2025_11_brueche;
-    static class Program
-    {
-        internal static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World als Klasse!");
-            foreach (var s in args)
-            {
-                Console.WriteLine(s);
-            }
 
-            var b1 = new Bruch(args[0]);
-            var b2 = new Bruch(args[1]);
-            var b3 = b1.Addiere(b2);
-            Console.WriteLine("Ergebnis: " + b3);
-        }
+internal static class Program
+{
+    internal static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World als Klasse!");
+        foreach (var s in args) Console.WriteLine(s);
+
+        var b1 = new Bruch(args[0]);
+        var b2 = new Bruch(args[1]);
+        var b3 = b1.Addiere(b2);
+        Console.WriteLine("Ergebnis: " + b3);
     }
+}
